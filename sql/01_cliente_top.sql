219a82be920a27ae86f03da1a02fcc3ca4b656d7
Ender-Perez <155691216+Ender-Perez@users.noreply.github.com>
2026-02-18 13:44:44 -0300

    Update 01_cliente_top.sql

--- sql/01_cliente_top.sql
+++ sql/01_cliente_top.sql
@@ -2,11 +2,11 @@
 
 SELECT 
     c.customer_id,
-    c.first_name,
-    c.last_name,
-    SUM(oi.quantity * oi.unit_price) AS total_spent
+    c.first_name || ' ' || c.last_name as cliente,
+    COUNT(o.order_id) AS total_ordenes,
+    SUM(oi.quantity * oi.unit_price) AS total_generado
 FROM customers c
 JOIN orders o ON c.customer_id = o.customer_id
 JOIN order_items oi ON o.order_id = oi.order_id
 GROUP BY c.customer_id
-ORDER BY total_spent DESC;
+ORDER BY total_generado DESC
